f3cd2cfbce891df6576b36b766b5e4b719413474
Alejandro Such Berenguer <alejandro.such@gmail.com>
2017-03-19 20:02:51 +0100

    Using renderer to add properties to the native element

--- src/directives/asb-draggable_directive.ts
+++ src/directives/asb-draggable_directive.ts
@@ -1,8 +1,4 @@
-import {
-  Directive, Input, ElementRef,
-  OnChanges, SimpleChange, Attribute,
-  HostListener
-} from '@angular/core';
+import {Attribute, Directive, ElementRef, HostListener, Input, OnChanges, Renderer, SimpleChange} from "@angular/core";
 
 @Directive({
   selector: '[asbDraggable]'
@@ -11,26 +7,25 @@ export class AsbDraggableDirective implements OnChanges {
   @Input('asbDraggable') isDraggable: boolean;
   @Input('asbDraggableData') data: any;
 
-  private nativeElement: any;
-
   @HostListener('dragstart', ['$event'])
-  onDragStart (event) {
+  onDragStart(event) {
     event.dataTransfer.effectAllowed = 'move';
     event.dataTransfer.setData(this.draggableType, this.draggableType);
     event.dataTransfer.setData('data', JSON.stringify(this.data));
   }
 
-  constructor (private element: ElementRef, @Attribute('asbDraggableType') private draggableType) {
-    this.nativeElement = element.nativeElement;
+  constructor(private element: ElementRef,
+              @Attribute('asbDraggableType') private draggableType,
+              private renderer: Renderer) {
   }
 
-  ngOnChanges (changes: { [propName: string]: SimpleChange }) {
+  ngOnChanges(changes: { [propName: string]: SimpleChange }) {
     let change: SimpleChange = changes['isDraggable'];
 
     if (change && change.currentValue) {
-      this.nativeElement.setAttribute('draggable', 'true');
+      this.renderer.invokeElementMethod(this.element.nativeElement, 'setAttribute', ['draggable', 'true']);
     } else {
-      this.nativeElement.setAttribute('draggable', 'false');
+      this.renderer.invokeElementMethod(this.element.nativeElement, 'setAttribute', ['draggable', 'false']);
     }
   }
 }
